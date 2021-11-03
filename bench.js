756ff9ce03cbc50452b04b4d32f53de7e6fa5b25
Dominic <@EMovhfIrFk4NihAKnRNhrfRaqIhBv1Wj8pTxJNgvCCY=.ed25519>
2021-11-03 15:58:53 +1300

    use high perf timer

--- bench.js
+++ bench.js
@@ -1,6 +1,8 @@
 var quicksort = require('./')
 quicksort.grow(500)
+var performance = require('perf_hooks').performance
 
+console.log('n, js, wasm, js/wasm')
 for(var N = 1; N <= 10000000; N = Math.ceil(N* 1.5)) {
   var b = new Buffer(N*4)
   var ary = new Int32Array(b.buffer), _ary = []
@@ -8,15 +10,16 @@ for(var N = 1; N <= 10000000; N = Math.ceil(N* 1.5)) {
   for(var i = 0; i < N; i++)
     _ary.push(ary[i] = (Math.random()*(1<<16))|0)
 
-  var start = Date.now()
+  var start = performance.now()
   _ary.sort(function (a, b) { return a - b })
-  var js_time = Date.now() - start
+  var js_time = performance.now() - start
 
-  var start = Date.now()
-    b.copy(quicksort.buffer)
-    quicksort.sort(0, N*4 - 4)
-  var wa_time = Date.now() - start
-  console.log(N, js_time, wa_time)
+
+  var start = performance.now()
+  b.copy(quicksort.buffer)
+  quicksort.sort(0, N*4 - 4)
+  var wa_time = performance.now() - start
+  console.log([N, js_time, wa_time, js_time/wa_time].join(', '))
 }
 
 
